aec7b5e1b2ec340330be471e3b36035ec6f77936
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-05-26 17:29:26 +0530

    Update Train.py

--- src/losses/Train.py
+++ src/losses/Train.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision
 import numpy as np
 from tqdm import tqdm
 from torch.optim.lr_scheduler import ReduceLROnPlateau
