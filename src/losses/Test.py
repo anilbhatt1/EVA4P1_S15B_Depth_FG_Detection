03bfdeb331d4844979a1cfd428e3b6e613fcd018
Anilkumar N Bhatt <43835604+anilbhatt1@users.noreply.github.com>
2020-05-26 16:07:39 +0530

    Add files via upload

--- src/losses/Test.py
+++ src/losses/Test.py
@@ -11,9 +11,9 @@ from kornia.losses import SSIM
 from kornia.losses import DiceLoss
 
 # # class for Calculating and storing testing losses and testing accuracies of model for each epoch ## 
-class Test_loss:
+class Testing_loss:
 
-      def test_loss_calc(self,model, device, test_loader, optimizer, epoch, criterion1, criterion2, path_name, scheduler=None, img_save_idx =500):
+      def test_loss_calc(self,model, device, test_loader, optimizer, epoch, criterion1, criterion2, batch_size, path_name, scheduler=None, img_save_idx =500):
           self.model        = model
           self.device       = device
           self.test_loader  = test_loader
@@ -22,20 +22,22 @@ class Test_loss:
           self.criterion1   = criterion1
           self.criterion1   = criterion2           
           self.scheduler    = scheduler
+          self.batch_size   = batch_size
           self.path_name    = path_name
           self.img_save_idx = img_save_idx
 
           model.eval()  
-          test_loss1, test_loss2, test_loss = 0, 0, 0
+          test_loss1, test_loss2, test_loss, test_mask_iou_cum, test_depth_iou_cum = 0, 0, 0, 0, 0
           pbar = tqdm(test_loader)
+          num_batches = len(test_loader.dataset)/batch_size
           cuda0 = torch.device('cuda:0')
 
           with torch.no_grad():
             for batch_idx, data in enumerate(pbar):
-              data['f1'] = data['f1'].to(device)
-              data['f2'] = data['f2'].to(device)
-              data['f3'] = data['f3'].to(device)
-              data['f4'] = data['f4'].to(device)      
+              data['f1'] = data['f1'].to(cuda0)
+              data['f2'] = data['f2'].to(cuda0)
+              data['f3'] = data['f3'].to(cuda0)
+              data['f4'] = data['f4'].to(cuda0)      
               data['f3O'] = torch.tensor(data['f3'],dtype= torch.int64, device= cuda0)      
             
               output = model(data)
@@ -46,36 +48,40 @@ class Test_loss:
               test_loss1 += loss1
               test_loss2 += loss2
               test_loss  += loss
+              mask_iou   = self.calculate_iou(data['f3O'].detach().cpu().numpy(), output[0].detach().cpu().numpy())
+              depth_iou  = self.calculate_iou(data['f4'].detach().cpu().numpy(),  output[1].detach().cpu().numpy())
+              test_mask_iou_cum  += mask_iou
+              test_depth_iou_cum += depth_iou
 
-              pbar.set_description(desc = f'{int(epoch)} {int(batch_idx)} tl={loss.item():.4f} tl1={loss1.item():.5f} tl2={loss2.item():.5f}')   
+              pbar.set_description(desc = f'TS{int(epoch)}|{int(batch_idx)}|{loss1:.3f}|{loss2:.3f}|{mask_iou:.3f}|{depth_iou:.3f}')   
               
-              if batch_idx % save == 0:
-                  print('Test Epoch: {} [{}/{} ({:.0f}%)]\tTest_Loss: {:.6f} Mask_Loss: {:.5f} Dpth_Loss: {:.5f}'.format(
-                        epoch, batch_idx * len(data), len(test_loader.dataset), (100. * batch_idx / len(test_loader)),
-                        test_loss, test_loss1, test_loss2))
-                  draw_and_save_tst(output[0].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_MP_{loss.item():.5f}.jpg')
-                  draw_and_save_tst(data['f3'].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_MA_{loss.item():.5f}.jpg')
-                  draw_and_save_tst(output[1].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_DP_{loss.item():.5f}.jpg')
-                  draw_and_save_tst(data['f4'].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_DA_{loss.item():.5f}.jpg')
-                  draw_and_save_tst(data['f1'].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_FGBG_{loss.item():.5f}.jpg')       
+              if batch_idx % img_save_idx == 0:
+                  print('Test Epoch: {} [{}/{} ({:.0f}%)]\tTest_Loss: {:.6f} Mask_Loss: {:.5f} Dpth_Loss: {:.5f} Mask_IOU: {:.5f} Dpth_IOU: {:.5F}'
+                         .format(epoch, batch_idx * len(data), len(test_loader.dataset), (100. * batch_idx / len(test_loader)),
+                                 loss.item(), loss1.item(), loss2.item(),mask_iou, depth_iou ))
+                  #draw_and_save(output[0].detach().cpu(),  f'{path_name}Test_{epoch}_{batch_idx}_MP_{loss.item():.5f}.jpg')
+                  #draw_and_save(data['f3'].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_MA_{loss.item():.5f}.jpg')
+                  #draw_and_save(output[1].detach().cpu(),  f'{path_name}Test_{epoch}_{batch_idx}_DP_{loss.item():.5f}.jpg')
+                  #draw_and_save(data['f4'].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_DA_{loss.item():.5f}.jpg')
+                  #draw_and_save(data['f1'].detach().cpu(), f'{path_name}Test_{epoch}_{batch_idx}_FGBG_{loss.item():.5f}.jpg')       
             
-          test_loss /= len(test_loader.dataset)
-          mask_loss = test_loss1/len(test_loader.dataset)
-          dpth_loss = test_loss2/len(test_loader.dataset)
-          return test_loss, mask_loss, dpth_loss     
-              
-      def draw_and_save_tst(tensors, name, figsize=(15,15), *args, **kwargs):
-          try:
-             tensors = tensors.detach().cpu()
-          except:
-             pass
-          grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)
-          grid_image  = grid_tensor.permute(1, 2, 0)
-          plt.figure(figsize = figsize)
-          plt.imshow(grid_image)
-          plt.xticks([])
-          plt.yticks([])
+          #test_loss      /= len(test_loader.dataset)
+          test_loss      /= num_batches
+          test_mask_loss  = test_loss1/num_batches
+          test_depth_loss = test_loss2/num_batches
+          test_mask_iou   = test_mask_iou_cum/num_batches
+          test_depth_iou  = test_depth_iou_cum/num_batches
+          return test_loss, test_mask_loss, test_depth_loss, test_mask_iou, test_depth_iou
 
-          plt.savefig(name, bbox_inches='tight')
-          plt.close()
-         #plt.show()              
+      def calculate_iou(self, target, prediction, thresh=0.5):
+        '''
+        Calculate intersection over union value
+        :param target: ground truth
+        :param prediction: output predicted by model
+        :param thresh: threshold
+        :return: iou value
+        '''
+        intersection = np.logical_and(np.greater(target, thresh), np.greater(prediction, thresh))
+        union = np.logical_or(np.greater(target, thresh), np.greater(prediction, thresh))
+        iou_score = np.sum(intersection) / np.sum(union)
+        return iou_score             
--- src/losses/Train.py
+++ src/losses/Train.py
@@ -11,40 +11,7 @@ from kornia.losses import SSIM
 from kornia.losses import DiceLoss
 
 # # class for Calculating and storing training losses and training accuracies of model for each batch per epoch ## 
-class Train_loss:
-    
-    def draw_show_and_save(tensors, name, figsize=(15,15), *args, **kwargs):
-        try:
-           tensors = tensors.detach().cpu()
-        except:
-           pass
-        grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)
-        grid_image  = grid_tensor.permute(1, 2, 0)
-        plt.figure(figsize = figsize)
-        plt.imshow(grid_image)
-        plt.xticks([])
-        plt.yticks([])
-
-        plt.savefig(name, bbox_inches='tight')
-        plt.show()
-        
-    def draw_and_save(tensors, name, figsize=(15,15), *args, **kwargs):
-        try:
-           tensors = tensors.detach().cpu()
-        except:
-           pass
-        grid_tensor = torchvision.utils.make_grid(tensors, *args, **kwargs)
-        grid_image  = grid_tensor.permute(1, 2, 0)
-        plt.figure(figsize = figsize)
-        plt.imshow(grid_image)
-        plt.xticks([])
-        plt.yticks([])
-
-        plt.savefig(name, bbox_inches='tight')
-        plt.close()
-       #plt.show()
-    
-
+class Training_loss: 
     def train_loss_calc(self,model, device, train_loader, optimizer, epoch, criterion1, criterion2, batch_size, path_name,path_model_save,
                         scheduler=None, model_save_idx=500, img_save_idx=500,maxlr=0):
                         
@@ -64,16 +31,16 @@ class Train_loss:
           self.path_model_save = path_model_save
         
           model.train()
-          train_loss1, train_loss2, train_loss = 0, 0, 0
+          train_loss1, train_loss2, train_loss, train_mask_iou_cum, train_depth_iou_cum = 0, 0, 0, 0, 0
           pbar = tqdm(train_loader)
           num_batches = len(train_loader.dataset)/batch_size
           cuda0 = torch.device('cuda:0')           
 
           for batch_idx, data in enumerate(pbar):
-            data['f1'] = data['f1'].to(device)
-            data['f2'] = data['f2'].to(device)
-            data['f3'] = data['f3'].to(device)
-            data['f4'] = data['f4'].to(device)
+            data['f1'] = data['f1'].to(cuda0)
+            data['f2'] = data['f2'].to(cuda0)
+            data['f3'] = data['f3'].to(cuda0)
+            data['f4'] = data['f4'].to(cuda0)
             data['f3O'] = torch.tensor(data['f3'],dtype= torch.int64, device= cuda0)
 
             optimizer.zero_grad()
@@ -84,42 +51,48 @@ class Train_loss:
             loss  = 2*loss1 + loss2
             train_loss1 += loss1
             train_loss2 += loss2
-            train_loss  += loss            
-            
+            train_loss  += loss
+            mask_iou   = self.calculate_iou(data['f3O'].detach().cpu().numpy(), output[0].detach().cpu().numpy())
+            depth_iou  = self.calculate_iou(data['f4'].detach().cpu().numpy(),  output[1].detach().cpu().numpy())
+            train_mask_iou_cum  += mask_iou
+            train_depth_iou_cum += depth_iou
 
-            pbar.set_description(desc = f'{int(epoch)} {int(batch_idx)} l={loss.item():.4f} l1={loss1.item():.5f} l2={loss2.item():.5f}')   
+            pbar.set_description(desc = f'TR{int(epoch)}|{int(batch_idx)}|{loss1:.3f}|{loss2:.3f}|{mask_iou:.3f}|{depth_iou:.3f}') 
+                                          
             loss.backward()
             optimizer.step()
             
-            saved = False
-            if batch_idx == 0 or batch_idx == int(num_batches-1):
-                print('Train Epoch: {} Batch_ID: {} [{}/{} ({:.0f}%)]\tLoss: {:.5f} Mask_Loss: {:.5f} Dpth_Loss: {:.5f}'.format(
-                       epoch, batch_idx, batch_idx * batch_size, len(train_loader.dataset),
-                       (100. * batch_idx / len(train_loader)),
-                       loss, loss1, loss2))
-                draw_show_and_save(output[0].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_MP_{loss.item():.5f}.jpg')
-                draw_show_and_save(data['f3'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_MA_{loss.item():.5f}.jpg')
-                draw_show_and_save(output[1].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_DP_{loss.item():.5f}.jpg')
-                draw_show_and_save(data['f4'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_DA_{loss.item():.5f}.jpg')
-                draw_show_and_save(data['f1'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_FGBG_{loss.item():.5f}.jpg')      
-                saved = True
-
-            if batch_idx % img_save_idx == 0 and not saved:
-                print('Train Epoch: {} Batch_ID: {} [{}/{} ({:.0f}%)]\tLoss: {:.5f} Mask_Loss: {:.5f} Dpth_Loss: {:.5f}'.format(
-                       epoch, batch_idx, batch_idx * batch_size, len(train_loader.dataset), 
-                       (100. * batch_idx / len(train_loader)),
-                       loss, loss1, loss2))
-                draw_and_save(output[0].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_MP_{loss.item():.5f}.jpg')
-                draw_and_save(data['f3'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_MA_{loss.item():.5f}.jpg')
-                draw_and_save(output[1].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_DP_{loss.item():.5f}.jpg')
-                draw_and_save(data['f4'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_DA_{loss.item():.5f}.jpg')
-                draw_and_save(data['f1'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_FGBG_{loss.item():.5f}.jpg')    
+            if batch_idx % img_save_idx == 0 or batch_idx == int(num_batches-1):
+                print('Train Epoch:{} Batch_ID: {} [{}/{} ({:.0f}%)]\tLoss:{:.5f} Mask_Loss:{:.5f} Dpth_Loss:{:.5f} Mask_IOU:{:.5f} Dpth_IOU: {:.5F}'
+                      .format(epoch, batch_idx, batch_idx * batch_size, len(train_loader.dataset), (100. * batch_idx / len(train_loader)),
+                       loss, loss1, loss2, mask_iou, depth_iou))
+                #draw_and_save(output[0].detach().cpu(),  f'{path_name}{epoch}_{batch_idx}_MP_{loss.item():.5f}.jpg')
+                #draw_and_save(data['f3'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_MA_{loss.item():.5f}.jpg')
+                #draw_and_save(output[1].detach().cpu(),  f'{path_name}{epoch}_{batch_idx}_DP_{loss.item():.5f}.jpg')
+                #draw_and_save(data['f4'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_DA_{loss.item():.5f}.jpg')
+                #draw_and_save(data['f1'].detach().cpu(), f'{path_name}{epoch}_{batch_idx}_FGBG_{loss.item():.5f}.jpg')    
               
-            if batch_idx % model_idx == 0:
+            if batch_idx % model_save_idx == 0:
               torch.save(model.state_dict(),path_model_save)
               print('MODEL SAVED:',path_model_save, 'Epoch & Batch-ID:', epoch, batch_idx)
               
-          train_loss /= len(train_loader.dataset)
-          mask_train_loss = test_loss1/len(test_loader.dataset)
-          dpth_train_loss = test_loss2/len(test_loader.dataset)
-          return train_loss, mask_train_loss, dpth_train_loss                    
+          #train_loss       /= len(train_loader.dataset)
+          train_loss       /= num_batches
+          train_mask_loss   = train_loss1/num_batches
+          train_depth_loss  = train_loss2/num_batches
+          train_mask_iou    = train_mask_iou_cum/num_batches
+          train_depth_iou   = train_depth_iou_cum/num_batches 
+          return train_loss, train_mask_loss, train_depth_loss, train_mask_iou, train_depth_iou   
+
+    def calculate_iou(self, target, prediction, thresh=0.5):
+        '''
+        Calculate intersection over union value
+        :param target: ground truth
+        :param prediction: output predicted by model
+        :param thresh: threshold
+        :return: iou value
+        '''
+        intersection = np.logical_and(np.greater(target, thresh), np.greater(prediction, thresh))
+        union = np.logical_or(np.greater(target, thresh), np.greater(prediction, thresh))
+        iou_score = np.sum(intersection) / np.sum(union)
+        return iou_score
